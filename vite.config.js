8957c45e2dec0ed931ad4b613d2ac1383caf1737
Aviv Shtar <Avivshtar3@gmail.com>
2026-07-21 14:16:03 +0300

    perfect clean production build for vercel

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,14 @@ import { resolve } from 'path';
 
 export default defineConfig({
   build: {
-    cssCodeSplit: true,
+    cssCodeSplit: true, // מונע מ-CSS של דף אחד להרוס דף אחר
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
         israelInvestments: resolve(__dirname, 'israelInvestments.html'),
         apartments: resolve(__dirname, 'apartments.html'),
         item: resolve(__dirname, 'item.html'),
-        properties: resolve(__dirname, 'properties.html'), // 👈 הנה הקובץ שהיה חסר!
+        properties: resolve(__dirname, 'properties.html'),
       },
     },
   },
