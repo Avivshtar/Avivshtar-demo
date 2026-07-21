3f32caecc24ed1f67eb39f72e8fc96d8d44f57b1
Aviv Shtar <Avivshtar3@gmail.com>
2026-07-21 14:21:26 +0300

    Testing original HTML with manual Rollup inputs

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,23 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
 
 export default defineConfig({
   build: {
-    cssCodeSplit: true, // מונע מ-CSS של דף אחד להרוס דף אחר
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        israelInvestments: resolve(__dirname, 'israelInvestments.html'),
-        apartments: resolve(__dirname, 'apartments.html'),
-        item: resolve(__dirname, 'item.html'),
-        properties: resolve(__dirname, 'properties.html'),
-      },
-    },
-  },
+        main: './index.html',
+        properties: './properties.html',
+        israelInvestments: './israelInvestments.html',
+        apartments: './apartments.html',
+        item: './item.html',
+        // תוסיף כאן את כל קבצי ה-JS שלך כדי ש-Vite יארוז אותם
+        header: './header.js',
+        propertyCard: './propertyCard.js',
+        mapLogic: './mapLogic.js',
+        nextSection: './nextSection.js',
+        shortcutsLogic: './shortcuts-logic.js',
+        shortcutsLogic2: './shortcuts-logic-2.js',
+        shortcutsLogic3: './shortcuts-logic-3.js',
+      }
+    }
+  }
 });
